51cbd450f3cf556ac216777254ce43c916f43d1e
futo <faalekseev@maieducation.com>
2025-11-19 11:26:38 +0300

    Factorial & Fibo error check

--- src/factorial_fibo.py
+++ src/factorial_fibo.py
@@ -1,4 +1,9 @@
+from src.constants import ROOT
+
 def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError("Input can't be negative integer")
+
     result = 1
     for i in range(2, n + 1):
         result *= i
@@ -6,17 +11,25 @@ def factorial(n: int) -> int:
 
 
 def factorial_recursive(n: int) -> int:
+    if n < 0:
+        raise ValueError("Input can't be negative integer")
+
     if n == 0:
         return 1
     return n * factorial_recursive(n - 1)
 
 
 def fibo(n: int) -> int:
-    root = 5 ** 0.5
-    return int(((1 + root) ** n - (1 - root) ** n) / (root * 2 ** n))
+    if n <= 0:
+        raise ValueError("Input must me positive integer")
+
+    return round(((1 + ROOT) ** n - (1 - ROOT) ** n) / (ROOT * 2 ** n))
 
 
 def fibo_recursive(n: int) -> int:
+    if n <= 0:
+        raise ValueError("Input must me positive integer")
+
     if n == 1 or n == 2:
         return 1
     return fibo_recursive(n - 1) + fibo_recursive(n - 2)
