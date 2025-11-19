07392ba3aaa373ba9de168a2ce4cf72f7f0243c4
futo <faalekseev@maieducation.com>
2025-11-19 19:09:11 +0300

    Benchmark add. Include docstring

---
+++ src/benchmark.py
@@ -0,0 +1,31 @@
+import time
+
+def timeit_once(func, *args, **kwargs) -> float:
+    '''
+    Измерение времени работы сортировки для некоторого массива.
+    :param func: Фукнция (метод) сортировки.
+    :param args: Список для сортировки.
+    :param kwargs: Дополнительный аргумент для сортировки (для radix sort: base и bucker sort: buckets)
+    :return: Время, за которое завершилась сортировка.
+    '''
+    start = time.perf_counter()
+    func(*args, **kwargs)
+    end = time.perf_counter()
+    return end - start
+
+def benchmark_sorts(arrays: dict[str, list], algos: dict[str, callable]) -> dict[str, dict[str, float]]:
+    '''
+    Запуск бенчмарка для разных алгоритмов сортировки на различных заданных массивах.
+    :param arrays: Словарь измеряемых массивов.
+    :param algos: Словарь проверяемых алгоритмов.
+    :return: Словарь результатов.
+    '''
+    result = {}
+    for array_name, array in arrays.items():
+        results[array_name] = {}
+        for algo_name, algo_func in algos.items():
+            test_array = array.copy()
+            time_taken = timeit_once(algo_func, test_array)
+            results[array_name][algo_name] = time_taken
+
+    return result
